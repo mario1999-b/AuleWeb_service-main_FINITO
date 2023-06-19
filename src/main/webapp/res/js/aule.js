fcadba796dc8368e982c88b6bc0656381a6301e7
leoleo1010 <75475394+leoleo1010@users.noreply.github.com>
2023-06-19 17:56:58 +0200

    modifiche IMPORTANTI

--- src/main/webapp/res/js/aule.js
+++ src/main/webapp/res/js/aule.js
@@ -13,10 +13,53 @@ const aula_container = $('#aula-container');
 //END DICHIARAZIONE SELETTORI
 
 
-// OP.1 ELENCO CSV AULE
-function getCSVaule(){
+// OP.5 INFORMAZIONI DI BASE DI UN AULA
+function getInfAula(val){
     message("", "");
     clear();
     toggleVisibility(aula_container);
 
+    if (val) {
+        $.ajax({
+            url: "rest/aule/" + val,
+            method: "GET",
+            success: function (data) {
+                //data - aula
+                 aula_result.children().remove();
+                message("Aula caricata.", "success");
+                populateAula(data); //popolo la tabella con l'aula
+            },
+            error: function (request, status, error) {
+                handleError(request, status, error, "#aula", "Errore nel caricamento della collezione.");
+            },
+            cache: false,
+        });
+    } else {
+        handleError("", "", "", "#aula", "Input errato");
+    }
+}
+
+/*
+ Funzione Utility per il riempimento della tabella delle aule
+ */
+function populateAula(data) {
+    if (data) {
+        //Se la collezione non è vuota viene inserita nella tabella.
+        aula_result.show();
+        aula_empty.hide();
+
+        aula_result.append('<tr>')
+        aula_result.append('<td>' + data['id'] + '</td>')
+        aula_result.append('<td>' + data['nome'] + '</td>')
+        aula_result.append('<td>' + data['luogo'] + '</td>')
+        aula_result.append('<td>' + data['edificio'] + '</td>')
+        aula_result.append('<td>' + data['emailResponsabile'] + '</td>')
+        aula_result.append('<td>' + data['gruppo'] + '</td>')
+        aula_result.append('</tr>')
+    } else {
+        //Se l'aula è vuota viene svuotata la tabella e viene mostrato un messaggio.
+        aula_result.children().remove();
+        aula_empty.show();
+    }
+    aula_empty.text("Non ci sono aule.");
 }
