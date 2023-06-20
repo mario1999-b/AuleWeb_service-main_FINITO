53cdff15dded1dcdac39057ddf8e0426fe88cab3
leoleo1010 <75475394+leoleo1010@users.noreply.github.com>
2023-06-20 13:12:39 +0200

    modifiche IMPORTANTI 6

--- src/main/webapp/res/js/aule.js
+++ src/main/webapp/res/js/aule.js
@@ -5,10 +5,12 @@
 
 
 //DICHIARAZIONE SELETTORI
-
-const aula_result = $('#aula-result');
-const aula_empty = $('#aula-empty');
-const aula_container = $('#aula-container');
+const aula_result = $('#aule-result');
+const aula_empty = $('#aule-empty');
+const aula_container = $('#aule-container');
+const attrezzature_result = $('#attrezzature-result')
+const attrezzature_empty = $('#attrezzature-empty');
+const attrezzature_container = $('#attrezzature-container');
 
 //END DICHIARAZIONE SELETTORI
 
@@ -25,7 +27,7 @@ function getInfAula(val){
             method: "GET",
             success: function (data) {
                 //data - aula
-                 aula_result.children().remove();
+                 $(aula_result).children().remove();
                 message("Aula caricata.", "success");
                 populateAula(data); //popolo la tabella con l'aula
             },
@@ -42,9 +44,10 @@ function getInfAula(val){
 /*
  Funzione Utility per il riempimento della tabella delle aule
  */
+
+
 function populateAula(data) {
     if (data) {
-        //Se la collezione non è vuota viene inserita nella tabella.
         aula_result.show();
         aula_empty.hide();
 
@@ -58,8 +61,59 @@ function populateAula(data) {
         aula_result.append('</tr>')
     } else {
         //Se l'aula è vuota viene svuotata la tabella e viene mostrato un messaggio.
-        aula_result.children().remove();
+        $(aula_result).children().remove();
         aula_empty.show();
     }
     aula_empty.text("Non ci sono aule.");
+}
+
+// OP.5 ATTREZZATURE DI UN AULA
+function getListAttAula(val){
+    message("", "");
+    clear();
+    toggleVisibility(attrezzature_container);
+
+    if (val) {
+        $.ajax({
+            url: "rest/aule/" + val + "/attrezzature",
+            method: "GET",
+            success: function (data) {
+                //data - aula
+                $(attrezzature_result).children().remove();
+                message("Attrezzatura caricata.", "success");
+                populateAttrezzature(data); //popolo la tabella con l'attrezzatura
+            },
+            error: function (request, status, error) {
+                handleError(request, status, error, "#attrezzature", "Errore nel caricamento della collezione.");
+            },
+            cache: false,
+        });
+    } else {
+        handleError("", "", "", "#attrezzature", "Input errato");
+    }
+}
+
+/*
+ Funzione Utility per il riempimento della tabella delle attrezzature
+ */
+
+
+function populateAttrezzature(data) {
+    if (data) {
+
+        attrezzature_result.show();
+        attrezzature_empty.hide();
+
+        attrezzature_result.append('<tr>')
+        attrezzature_result.append('<td>' + data['id'] + '</td>')
+        attrezzature_result.append('<td>' + data['n_tavoli'] + '</td>')
+        attrezzature_result.append('<td>' + data['n_sedie'] + '</td>')
+        attrezzature_result.append('<td>' + data['n_lavagne'] + '</td>')
+        attrezzature_result.append('</tr>')
+    } else {
+        //Se l'aula è vuota viene svuotata la tabella e viene mostrato un messaggio.
+        $(attrezzature_result).children().remove();
+        attrezzature_empty.show();
+    }
+    attrezzature_empty.text("Non ci sono attrezzature.");
 }
